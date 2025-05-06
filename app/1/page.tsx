1d9a390fd3ec767a889196fbe18a651cb93c55b1
Tung Le <tung.le@advocado.me>
2025-05-06 23:58:21 +0700

    fix size

--- app/1/page.tsx
+++ app/1/page.tsx
@@ -97,7 +97,7 @@ export default function Home() {
             <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
               <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
                 <div className="relative transform overflow-hidden rounded-lg bg-blue-300/70 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
-                  <div className="px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                  <div className="px-4 pt-5 pb-4 sm:p-6 sm:pb-4 aspect-square flex items-center justify-center">
                     <div className="sm:flex sm:items-start">
                       <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
                         <h3 className="text-base font-semibold text-white" id="modal-title">Thông tin trúng thưởng</h3>
